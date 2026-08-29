bfd17bab25d68e943e308266ccddd5a9ef63e3b3
Aitor <aitor19rp@gmail.com>
2026-08-29 22:57:41 +0200

    feat: add site data and home page

---
+++ _data/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+  nombre: "La Maleta Lista",
+  tagline: "Cada semana busco las mejores ofertas de vuelos, hoteles y escapadas por Europa para que tú solo tengas que hacer la maleta.",
+  redes: {
+    tiktok: "https://www.tiktok.com/@TU_USUARIO_TIKTOK",
+    instagram: "https://www.instagram.com/TU_USUARIO_INSTAGRAM",
+    facebook: "https://www.facebook.com/TU_PAGINA_FACEBOOK"
+  },
+  telegram: "https://t.me/TU_CANAL_TELEGRAM"
+};
