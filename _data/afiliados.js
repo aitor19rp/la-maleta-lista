5e0f258ef539397bc048c38f6b683b9cf351da9d
Aitor <aitor19rp@gmail.com>
2026-08-30 12:21:34 +0200

    feat: add real DiscoverCars affiliate link

--- _data/afiliados.js
+++ _data/afiliados.js
@@ -1,4 +1,4 @@
 module.exports = {
   seguro: "https://www.heymondo.es/TU_ENLACE_AFILIADO_HEYMONDO",
-  coche: "https://www.discovercars.com/es?affiliate=TU_ID_AFILIADO_DISCOVERCARS"
+  coche: "https://www.discovercars.com/?a_aid=lamaletalista"
 };
