3eca5fe57ecf7853818b26f4757f7da6eaa704df
Aitor <aitor19rp@gmail.com>
2026-08-31 20:18:11 +0200

    fix: hide seguro de viaje card until a real affiliate link exists

--- _data/afiliados.js
+++ _data/afiliados.js
@@ -1,4 +1,5 @@
 module.exports = {
+  seguroActivo: false, // pon esto en true en cuanto tengas un link de afiliado real (Heymondo/IATI)
   seguro: "https://www.heymondo.es/TU_ENLACE_AFILIADO_HEYMONDO",
   coche: "https://www.discovercars.com/?a_aid=lamaletalista"
 };
